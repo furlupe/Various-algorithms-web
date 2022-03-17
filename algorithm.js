27ead8dc6563b20114ecf3a755d7a3e8cb38a5b8
krigggg <shumanka2004@gmail.com>
2022-03-17 17:52:07 +0700

    ввод графа

---
+++ algorithm.js
@@ -0,0 +1,20 @@
+import {circles} from './canvas.js';
+function geneticAlgorithm(){
+    let generations = [];
+    console.log(circles);
+
+}
+
+// function firstGeneration (circles) {
+//     return shuffle(circles);
+// }
+
+// function distance(x1, y1, x2, y2) {
+//     let xDist;
+//     let yDist;
+//     let Dist;
+//     xDist = Math.abs(x1 - x2);
+//     yDist = Math.abs(y1 - y2);
+//     Dist = Math.sqrt((Math.pow(xDist, 2)) + (Math.pow(yDist, 2)));
+//     return Dist;
+// }
--- canvas.js
+++ canvas.js
@@ -1,16 +1,65 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContex('2d');
+window.onload = function() {
+    // Определение контекста рисования
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");  
+}
 
-var mouse ={x:0, y:0};
-var pointSize = 3;
+let circles = [];
+let id=0;
 
-canvas.addEventListener("click", e =>{
-    mouse.x = e.offsetX;
-    mouse.y =e.offsetY;
+class Circle {
+    constructor(x , y, radius, color) {
+        this.id=id;
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.isSelected = false;
+    }
+}
 
-    ctx.beginPath();
-    ctx.arc(mouse.x, mouse.y, pointSize, 50, 50);
-    ctx.fillStyle = ctx.strokeStyle = "black";
-    ctx.fill();
-    ctx.stroke();
-});
+canvas.onmousedown = function(event){
+    let x = event.offsetX || 0;
+    let y = event.offsetY || 0;
+    id= ++id;
+    // Создаем новый круг
+    let circle = new Circle(x, y, 20, "pink");
+    // Сохраняем его в массиве
+    circles.push(circle);
+    console.log(circles);
+    drawCircles();
+    if (circles.length >= 2){
+        drawLine();
+    }
+}
+
+function drawCircles() {
+    // Перебираем все круги
+    for(let i=0; i<circles.length; i++) {
+        let circle = circles[i];
+        // Рисуем текущий круг
+        context.beginPath();
+        context.arc(circle.x, circle.y, circle.radius, 0, Math.PI*2);
+        context.fillStyle = circle.color;
+        context.strokeStyle = "black";
+        context.fill();
+        context.stroke(); 
+    }
+}
+
+function drawLine() {
+    let x0 = circles[circles.length-1].x;
+    let y0 = circles[circles.length-1].y;
+    for(let i=circles.length-2; i>=0; i--) {
+        let x1 = circles[i].x;
+        let y1 = circles[i].y;
+        // Рисуем линии
+        context.beginPath();
+        context.moveTo(x0, y0); 
+        context.lineTo(x1, y1);
+        context.lineWidth = 3;         // толщина
+        context.strokeStyle = 'black'; // цвет
+        context.fill();
+        context.stroke(); 
+    }
+}
