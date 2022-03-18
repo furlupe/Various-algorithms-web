9449863a55a1b1ac3f72031b478fb565a5c45404
furlupe <228133713372282004@stud.tsu.ru>
2022-03-18 16:34:45 +0700

    функция сигмоиды и ее производная

--- js/network.js
+++ js/network.js
@@ -1,17 +1,18 @@
 // класс, определяющий нейросеть
 class Network {
 
-    datasetSize = 28;
+    datasetSize = 28; // Размер входных данных
+    sigA = 1.0; // коэфициент для сигмоиды
 
-    layerNeuronsN = [this.datasetSize ** 2, 25, 10]; // кол-во входов (изображение 50*50 = 2500), размер внутр. слоя и кол-во выходов (цифры 0-9)
+    layerNeuronsN = [this.datasetSize ** 2, 25, 10]; // кол-во входов (изображение размером datasetSize ^ 2), размер внутр. слоя и кол-во выходов (цифры 0-9)
 
     layerN = this.layerNeuronsN.length; // кол-во слоев
 
-    weights = new Array();
-    biases = new Array();
+    weights = new Array(); // матрциа весов
+    biases = new Array(); // матрица смещений
 
     constructor() {
-        // инициализировать матрицу весов
+        // инициализировать матрицу весов и матрицу смещений
         for(let layer = 1; layer < this.layerN; layer++) {
 
             // в каждом слое создаем список нейронов
@@ -24,12 +25,22 @@ class Network {
                 // в каждом нейроне создаем список, где указываем веса связей оного с каждым нейроном из предыдущего слоя
                 this.weights[layer - 1][neuron] = new Array();
                 for (let prevNeuron = 0; prevNeuron < this.layerNeuronsN[layer - 1]; prevNeuron++) {
-                    this.weights[layer - 1][neuron][prevNeuron] = Math.random();
+                    this.weights[layer - 1][neuron][prevNeuron] = Math.random(); // заполняем случайные числа в диапазоне [0; 1)
                 }
 
-                this.biases[layer - 1][neuron] = Math.random() * 10;
+                this.biases[layer - 1][neuron] = Math.random() * 10; // случайные числа в диапазоне [0; 10)
             }
         }
+    }
+
+    // функция активации нейрона - сигмоида
+    sigmoid(x) {
+        return 1 / (1 + Math.exp( -this.sigA * x ));
+    }
+
+    // производная сигмоиды
+    sidmoidPrime(x) {
+        return this.sigmoid(x) * (1 - this.sigmoid(x));
     }
 }
 
