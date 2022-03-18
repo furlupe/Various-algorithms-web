e795000b9beeeb42ee8910cb86ce3f0f226358a3
krigggg <shumanka2004@gmail.com>
2022-03-19 00:09:25 +0700

    update not at all

--- algorithm.js
+++ algorithm.js
@@ -1,20 +1,61 @@
-import {circles} from './canvas.js';
-function geneticAlgorithm(){
-    let generations = [];
-    console.log(circles);
-
-}
-
-// function firstGeneration (circles) {
-//     return shuffle(circles);
+// import {circles} from 'canvas.js';
+// function geneticAlgorithm(){
+//     let generations = []; //массив где будут храниться все наши поколения (пути)
+//     let fitness = []; //массив с длинами путей
+//     let countGenerations = countOfelement(circles.length); //генерация оптимального(нет) кол-ва предков
+//     for (let i = 0; i < countGenerations; i++) {
+//         generations[i] = firstGeneration(chromosome); //генерация первого поколения 
+//     }
+//     for (let i = 0; i < generations.length; i++) {
+//         fitness[i] = distance(generations[i]); //подсчет длин путей для каждого предка
+//     }
 // }
 
-// function distance(x1, y1, x2, y2) {
-//     let xDist;
-//     let yDist;
-//     let Dist;
-//     xDist = Math.abs(x1 - x2);
-//     yDist = Math.abs(y1 - y2);
-//     Dist = Math.sqrt((Math.pow(xDist, 2)) + (Math.pow(yDist, 2)));
-//     return Dist;
+// //нахождение оптимального(нет) кол-ва предков
+// function countOfelement(length) {
+//     if (length <= 3) return length;
+//     let count = 1;
+//     for (let i = 3; i <= length; i++) {
+//         count *= i;
+//     }
+//     return count;
+// }
+
+// //генерация первого поколения
+// function firstGeneration (chromosome) {
+//     let editChromosome = [];
+//     editChromosome = chromosome.slice();
+//     editChromosome.shift();
+//     shuffle(editChromosome);
+//     editChromosome.push(chromosome[0]);
+//     editChromosome.unshift(chromosome[0]);
+//     return editChromosome;
+// }
+
+// //нахождение дистанции(фитнесса)
+// function distance(chromosome) {
+//     let resultDistance = 0;
+//     for (let i = 0; i < chromosome.length - 1; i++) { //расстояние от точки до точки
+//         let xDist;
+//         let yDist;
+//         let Dist;
+//         xDist = Math.abs(chromosome[i].x - chromosome[i + 1].y);
+//         yDist = Math.abs(chromosome[i].y - chromosome[i + 1].y);
+//         Dist = Math.sqrt((Math.pow(xDist, 2)) + (Math.pow(yDist, 2)));
+//         resultDistance += Dist;
+//     }
+//     return resultDistance;
+// }
+
+// //перемешивание массива
+// function shuffle(array) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//       let j = Math.floor(Math.random() * (i + 1));
+//       [array[i], array[j]] = [array[j], array[i]];
+//     }
+// }
+
+// //кроссинговер или скрещивание()
+// function crossing(ancestor1, ancestor2) {
+
 // }
--- canvas.js
+++ canvas.js
@@ -2,6 +2,10 @@ window.onload = function() {
     // Определение контекста рисования
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");  
+    context.beginPath();
+    context.rect(0, 0, 900, 700);
+    context.fillStyle = "white";
+    context.fill();
 }
 
 let circles = [];
@@ -61,5 +65,143 @@ function drawLine() {
         context.strokeStyle = 'black'; // цвет
         context.fill();
         context.stroke(); 
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let flag = false;
+
+function geneticAlgorithm(){
+    let generations = []; //массив где будут храниться все наши поколения (пути)
+    let fitness = []; //массив с длинами путей
+    let countElement = numberOfelement(circles.length - 1); //генерация оптимального(нет) кол-ва предков
+
+    for (let i = 0; i < countElement; i++) { //генерация первого поколения 
+        generations[i] = firstGeneration(generations); 
+        if (flag == true) {
+            generations.pop;
+            continue;
+        }
+    }
+
+    for (let i = 0; i < generations.length; i++) { //подсчет длин путей для каждого предка
+        fitness[i] = distance(generations[i]); 
+    }
+
+    if(fitness.length == 2 && fitness[0] == fitness[1]) {
+        console.log(generations[0]);
+        console.log(fitness[0]);
+    }
+
+    
+}
+
+//генерация первого поколения
+function firstGeneration (generations) {
+    let editChromosome = [];
+
+    editChromosome = circles.slice();
+    editChromosome.shift();
+    shuffle(editChromosome);
+    editChromosome.push(circles[0]);
+    editChromosome.unshift(circles[0]);
+
+    //проверка на наличие
+    for(let i = 0; i < generations.length; i++) {
+        if (generations[i] == editChromosome) { 
+            flag = true; 
+            break;
+        }
+    }
+
+    return editChromosome;
+}
+
+//кроссинговер или скрещивание
+function crossoverBest(generation, fitness) {
+    // let indexMin;
+    // let indexPreMin;
+    // let copy = fitness.slice();
+
+    // copy.sort(function(a, b) {
+    //     return a - b;
+    // });
+
+    // for(let i = 0; i < fitness.length; i++) {
+    //     if(fitness[i] == copy[0]) {
+    //         indexMin = i;
+    //     }
+    //     if(fitness[i] == copy[1]){
+    //         indexPreMin = i;
+    //     }
+    // }
+
+    //рандомно выбираем родителей
+    let ancestor1 = generation[getRandomInt(generation.length - 1)];
+    let ancestor2 = generation[getRandomInt(generation.length - 1)];
+    // let length = ancestor1.length - 2;
+    let child1;
+    let child2;
+    child1 = ancestor1.slice();
+
+}
+
+//нахождение дистанции(фитнесса)
+function distance(chromosome) {
+    let resultDistance = 0;
+
+    for (let i = 0; i < chromosome.length - 1; i++) { //расстояние от точки до точки
+        let xDist;
+        let yDist;
+        let Dist;
+
+        xDist = Math.abs(chromosome[i].x - chromosome[i + 1].x);
+        yDist = Math.abs(chromosome[i].y - chromosome[i + 1].y);
+        Dist = Math.sqrt((Math.pow(xDist, 2)) + (Math.pow(yDist, 2)));
+        resultDistance += Dist;
+    }
+
+    return resultDistance;
+}
+
+//перемешивание массива
+function shuffle(array) {
+
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+//нахождение оптимального(нет) кол-ва предков
+function numberOfelement(length) {
+    let fact = 1;
+    if(length <= 3) {
+        return 2;
+    }
+    for(let i = length; i > length - 2; i--){
+        fact *= i;
+    }
+    return fact;
+}
+
+//среднее значение фитнесса
+function averageFitness(distances) {
+    let sumDist = 0;
+    for(let i = 0; i < distances.length; i++) {
+        sumDist += distances[i];
+    }
+    return sumDist / distances.length;
+}
+
+//рандом
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+//выбор кол-ва генов для кроссинговера
+function numberOfGenes(lengthOfChromosome) {
+    if(lengthOfChromosome % 3 == 0) {
+
     }
 }
