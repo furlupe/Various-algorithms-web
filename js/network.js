3e31a8d3b0d1af4993c0693a6e9b2884bf5d7e73
furlupe <228133713372282004@stud.tsu.ru>
2022-03-21 10:54:44 +0700

    пару строк

--- js/network.js
+++ js/network.js
@@ -33,6 +33,48 @@ class Network {
         }
     }
 
+    teach(data) {
+        let cost = 0;
+
+        // таблица весовых сумм
+        let values = new Array();
+
+        // расчет весовых сумм для каждого нейрона
+        for (let layer = 1; layer < this.layerN; layer++) {
+
+            values[layer - 1] = new Array();
+            for(let neuron = 0; neuron < this.layerNeuronsN[layer]; neuron++) {
+
+                for (let prevNeuron = 0; prevNeuron < this.layerNeuronsN[layer - 1]; prevNeuron++) {
+
+                    // если находимя на первом скрытом слое, то использовать входные данные
+                    if (layer == 1) {
+                        values[layer - 1][neuron] += this.weights[layer - 1][neuron][prevNeuron] * data[prevNeuron];
+                    }
+
+                    // иначе использовать данные с предыдущего слоя
+                    else {
+                        values[layer - 1][neuron] += this.weights[layer - 1][neuron][prevNeuron] * values[layer - 2][prevNeuron];
+                    }
+
+                }
+
+                // применить сдвиг
+                values[layer - 1][neuron] += this.biases[layer - 1][neuron];
+
+                // привести значение к интервалу [0; 1]
+                values[layer - 1][neuron] = this.sigmoid(values[layer - 1][neuron]);
+            }
+
+        }
+
+        /*
+        for (let neuron = 0; neuron < this.layerNeuronsN[this.layerN - 1]; neuron++) {
+            cost += (values[this.layerN - 1] - )
+        }*/
+
+        cost *= 1/2;
+    }
     // функция активации нейрона - сигмоида
     sigmoid(x) {
         return 1 / (1 + Math.exp( -this.sigA * x ));
